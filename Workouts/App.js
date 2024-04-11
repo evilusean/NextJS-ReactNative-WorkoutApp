21d46b115e23eff983f7cbd63909e1e9d74bcb10
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-04-11 00:35:41 -0300

    Update App.js
    added to stylesheet

--- Workouts/App.js
+++ Workouts/App.js
@@ -7,10 +7,12 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text style={{ fontSize: 24, fontWeight: "500" }}>{exercises.name}</Text>
-      <Text style={{ color: "dimgray" }}>
-        Muscle: {exercises.muscle} | Equipment: {exercises.equipment}
-      </Text>
+      <View style={styles.exerciseContainer}>
+        <Text style={styles.exerciseName}>{exercises.name}</Text>
+        <Text style={styles.exerciseSubtitle}>
+          {exercises.muscle.toUpperCase()} | {exercises.equipment.toUpperCase()}
+        </Text>
+      </View>
 
       <StatusBar style="auto" />
     </View>
@@ -20,8 +22,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "gainsboro",
     alignItems: "center",
     justifyContent: "center",
+  },
+  exerciseContainer: {
+    backgroundColor: "#fff",
+    padding: 10,
+    borderRadius: 10,
+    gap: 5,
+  },
+  exerciseName: {
+    fontSize: 24,
+    fontWeight: "500",
+  },
+  exerciseSubtitle: {
+    color: "dimgray",
   },
 });
