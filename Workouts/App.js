adc62d64701cafa34321a697f172335fb3880073
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-04-11 20:50:18 -0300

    Update App.js
    created a custom component to render items

--- Workouts/App.js
+++ Workouts/App.js
@@ -2,26 +2,24 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, FlatList } from "react-native";
 import exercises from "./assets/data/exercises.json";
 
-export default function App() {
-  const exercises = exercises[0];
+function ExerciseListItem({ item }) {
+  return (
+    <View style={styles.exerciseContainer}>
+      <Text style={styles.exerciseName}>{item.name}</Text>
+      <Text style={styles.exerciseSubtitle}>
+        {item.muscle.toUpperCase()} | {item.equipment.toUpperCase()}
+      </Text>
+    </View>
+  );
+}
 
+export default function App() {
   return (
     <View style={styles.container}>
       <FlatList
         data={exercises}
-        renderItem={() => {
-          return (
-            <View style={styles.exerciseContainer}>
-              <Text style={styles.exerciseName}>{exercises.name}</Text>
-              <Text style={styles.exerciseSubtitle}>
-                {exercises.muscle.toUpperCase()} |{" "}
-                {exercises.equipment.toUpperCase()}
-              </Text>
-            </View>
-          );
-        }}
+        renderItem={({ item }) => <ExerciseListItem item={item} />}
       />
-
       <StatusBar style="auto" />
     </View>
   );
