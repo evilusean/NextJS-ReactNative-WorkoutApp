330eed2c12c34475e579de0425cedf06f6d7b24b
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-04-13 02:29:19 -0300

    changed to jsx
    added _layout

---
+++ Workouts/src/app/[name].jsx
@@ -0,0 +1,36 @@
+import { View, Text, StyleSheet } from "react-native";
+import { useLocalSearchParams } from "expo-router";
+import exercises from "../../assets/data/exercises.json";
+
+export default function ExercuseDetailsScreen() {
+  const params = useLocalSearchParams();
+  const exercise = exercises.find((item = item.name == params.name));
+  if (!exercise) {
+    return <Text>Exercise not found</Text>;
+  }
+  return (
+    <View styles={styles.container}>
+      <Text style={styles.exerciseName}>{exercise.name}</Text>
+      <Text style={styles.exerciseSubtitle}>
+        <Text style={styles.subValue}>{exercise.muscle}</Text>
+        <Text style={styles.subValue}> {exercise.equipment}</Text>
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+  },
+  exerciseName: {
+    fontSize: 24,
+    fontWeight: "500",
+  },
+  exerciseSubtitle: {
+    color: "dimgray",
+  },
+  subValue: {
+    textTransform: "capitalize",
+  },
+});
