e94a510b8357317abc7663607d0348a3fbfa3bd8
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-04-13 02:38:11 -0300

    fixed bugs

--- Workouts/src/app/[name].jsx
+++ Workouts/src/app/[name].jsx
@@ -2,9 +2,9 @@ import { View, Text, StyleSheet } from "react-native";
 import { useLocalSearchParams } from "expo-router";
 import exercises from "../../assets/data/exercises.json";
 
-export default function ExercuseDetailsScreen() {
+export default function ExerciseDetailsScreen() {
   const params = useLocalSearchParams();
-  const exercise = exercises.find((item = item.name == params.name));
+  const exercise = exercises.find((item) => item.name == params.name);
   if (!exercise) {
     return <Text>Exercise not found</Text>;
   }
