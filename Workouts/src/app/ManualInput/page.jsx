561c89d0469688e26f86d37eaf973a14e60abbba
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-04-15 01:21:41 -0300

    mongoDB
    boilerplate for manual input into MongoDB

--- Workouts/src/app/ManualInput/page.jsx
+++ Workouts/src/app/ManualInput/page.jsx
@@ -1,3 +1,45 @@
-export default function Page() {
-  return <h1>Hello, Next.js!</h1>;
-}
+import React from "react";
+import { View, TextInput, Button } from "react-native";
+import { Formik } from "formik";
+import axios from "axios";
+
+const ExerciseForm = () => {
+  return (
+    <Formik
+      initialValues={{
+        name: "",
+        type: "",
+        muscle: "",
+        equipment: "",
+        difficulty: "",
+        instructions: "",
+      }}
+      onSubmit={(values) => {
+        axios
+          .post("Your_API_Endpoint", values)
+          .then((response) => {
+            console.log("Form data sent successfully:", response.data);
+          })
+          .catch((error) => {
+            console.error("Error sending form data:", error);
+          });
+      }}
+    >
+      {({ handleChange, handleBlur, handleSubmit, values }) => (
+        <View>
+          <TextInput
+            onChangeText={handleChange("name")}
+            onBlur={handleBlur("name")}
+            value={values.name}
+            placeholder="Name"
+          />
+          {/* Add more TextInput fields for type, muscle, equipment, difficulty, and instructions */}
+
+          <Button title="Submit" onPress={handleSubmit} />
+        </View>
+      )}
+    </Formik>
+  );
+};
+
+export default ExerciseForm;
