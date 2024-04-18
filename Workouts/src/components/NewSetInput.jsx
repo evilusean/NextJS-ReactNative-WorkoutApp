658deaed6f534be27b4d61cfb3c13f8af563c814
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-04-18 16:32:15 -0300

    Update NewSetInput.jsx
    created state variables that will save our reps/weight answers

--- Workouts/src/components/NewSetInput.jsx
+++ Workouts/src/components/NewSetInput.jsx
@@ -1,13 +1,28 @@
 import { View, Text, StyleSheet, TextInput, Button } from "react-native";
+import { useState } from "react";
 
 const NewSetInput = () => {
+  const [reps, setReps] = useState("");
+  const [weight, setWeight] = useState("");
+
   const addset = () => {
     console.warn("Add Set");
   };
+
   return (
     <View style={styles.container}>
-      <TextInput placeholder="Reps" styles={styles.input} />
-      <TextInput placeholder="Weight" styles={styles.input} />
+      <TextInput
+        value={reps}
+        onChangeText={setReps}
+        placeholder="Reps"
+        styles={styles.input}
+      />
+      <TextInput
+        value={weight}
+        onChangeText={setWeight}
+        placeholder="Weight"
+        styles={styles.input}
+      />
       <Button title="Add" onPress={addSet} />
     </View>
   );
