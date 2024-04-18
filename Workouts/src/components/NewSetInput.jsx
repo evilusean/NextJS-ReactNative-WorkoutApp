5081ee308ae88dc9bc2e3f72b826d74c747ee998
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-04-18 00:45:42 -0300

    Create NewSetInput.jsx
    boilolerplate

---
+++ Workouts/src/components/NewSetInput.jsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const NewSetInput = () => {
+  return (
+    <View>
+      <Text>New Set Input</Text>
+    </View>
+  );
+};
+
+export default NewSetInput;
