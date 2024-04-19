63352ccd3b34a10cfb8946372e53f0f5784741e6
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-04-19 13:59:33 -0300

    Update SetsList.jsx
    added how to get todays exercises

--- Workouts/src/components/SetsList.jsx
+++ Workouts/src/components/SetsList.jsx
@@ -1,9 +1,16 @@
 import { View, Text } from "react-native";
 import { useQuery } from "@tanstack/react-query";
 
-//CREATE A QUERY
-//REMOVE IF NOT NEEDED FROM [name].jsx
+//TODO:
 //HOW TO MAKE QUERY ONLY PULL FROM TODAYS DATE?
+//FIX .ENV VARIABLES AND ADD CORRECT QUERY TO CODE - CURRENT DOES LOCAL - NOT CONNECTING TO DB
+
+//BELOW IS HOW TO FETCH ON A SPECIFIC DATE, NEED TO ADD IT IN WITH EXERCISES
+//const fetchExercises = async () => {
+//    const today = new Date().toISOString().slice(0, 10); // Get today's date in YYYY-MM-DD format
+//    const response = await fetch(`mongodb://localhost:27017/myDatabase/exercises?date=${today}`);
+//    return response.json();
+//  };
 
 const SetsList = () => {
   const exercises = useQuery(["exercises"], async () => {
