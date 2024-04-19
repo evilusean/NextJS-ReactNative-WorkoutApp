53853488d70c022cc492a186f98738b7c8620c6b
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-04-19 13:50:01 -0300

    Update SetsList.jsx
    added a query to using react-query to query our DB and get all exercises

--- Workouts/src/components/SetsList.jsx
+++ Workouts/src/components/SetsList.jsx
@@ -3,11 +3,23 @@ import { useQuery } from "@tanstack/react-query";
 
 //CREATE A QUERY
 //REMOVE IF NOT NEEDED FROM [name].jsx
+const fetchExercises = async () => {
+  const response = await fetch(
+    "mongodb://localhost:27017/myDatabase/exercises"
+  );
+  return response.json();
+};
+
+const exercises = useQuery(["exercises"], fetchExercises);
 
 const SetsList = () => {
   return (
     <View>
-      <Text>SetsList</Text>
+      {exercises.data.map((exercise) => (
+        <Text key={exercise._id}>
+          {exercise.reps} Reps - {exercise.name} - {exercise.weight} Weight
+        </Text>
+      ))}
     </View>
   );
 };
