5eccdee02a660b36b6afa31580353a51eeb30653
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-04-19 14:06:05 -0300

    Update SetsList.jsx
    added date, removed old code

--- Workouts/src/components/SetsList.jsx
+++ Workouts/src/components/SetsList.jsx
@@ -5,13 +5,6 @@ import { useQuery } from "@tanstack/react-query";
 //HOW TO MAKE QUERY ONLY PULL FROM TODAYS DATE?
 //FIX .ENV VARIABLES AND ADD CORRECT QUERY TO CODE - CURRENT DOES LOCAL - NOT CONNECTING TO DB
 
-//BELOW IS HOW TO FETCH ON A SPECIFIC DATE, NEED TO ADD IT IN WITH EXERCISES
-//const fetchExercises = async () => {
-//    const today = new Date().toISOString().slice(0, 10); // Get today's date in YYYY-MM-DD format
-//    const response = await fetch(`mongodb://localhost:27017/myDatabase/exercises?date=${today}`);
-//    return response.json();
-//  };
-
 const SetsList = () => {
   const exercises = useQuery(["exercises"], async () => {
     const today = new Date().toISOString().slice(0, 10); // Get today's date in YYYY-MM-DD format
@@ -23,10 +16,10 @@ const SetsList = () => {
 
   return (
     <View>
+      <Text>Today's date: {today}</Text>
       {exercises.data.map((exercise) => (
         <Text key={exercise._id}>
-          {exercise.name} - {exercise.reps} reps - {exercise.weight} Weight -{" "}
-          {exercise.date}
+          {exercise.name} - {exercise.reps} reps - {exercise.weight} Weight -
         </Text>
       ))}
     </View>
