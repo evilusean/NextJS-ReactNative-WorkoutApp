9bc7339ac40ec7356bf0581e950431eef35a10f2
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-04-19 00:56:59 -0300

    Create SetsList.jsx
    boilolerplate

---
+++ Workouts/src/components/SetsList.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const SetsList = () => {
+  return (
+    <View>
+      <Text>SetsList</Text>
+    </View>
+  );
+};
+
+export default SetsList;
