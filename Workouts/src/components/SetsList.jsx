b55f924019bacd0abd3b58fb608d56ede0feeaf9
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-04-19 13:51:58 -0300

    Update SetsList.jsx
    fixed code, need to figure out how to query date

--- Workouts/src/components/SetsList.jsx
+++ Workouts/src/components/SetsList.jsx
@@ -3,21 +3,21 @@ import { useQuery } from "@tanstack/react-query";
 
 //CREATE A QUERY
 //REMOVE IF NOT NEEDED FROM [name].jsx
-const fetchExercises = async () => {
-  const response = await fetch(
-    "mongodb://localhost:27017/myDatabase/exercises"
-  );
-  return response.json();
-};
-
-const exercises = useQuery(["exercises"], fetchExercises);
+//HOW TO MAKE QUERY ONLY PULL FROM TODAYS DATE?
 
 const SetsList = () => {
+  const exercises = useQuery(["exercises"], async () => {
+    const response = await fetch(
+      "mongodb://localhost:27017/myDatabase/exercises"
+    );
+    return response.json();
+  });
+
   return (
     <View>
       {exercises.data.map((exercise) => (
         <Text key={exercise._id}>
-          {exercise.reps} Reps - {exercise.name} - {exercise.weight} Weight
+          {exercise.name} - {exercise.reps} reps - {exercise.weight} lbs
         </Text>
       ))}
     </View>
