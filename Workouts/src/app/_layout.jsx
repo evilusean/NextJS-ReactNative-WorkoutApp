83f3e957a74e07b211b89082130267029ea58504
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-04-19 22:15:16 -0300

    RIP page2.jsx, page3.jsx
    RIP

--- Workouts/src/app/_layout.jsx
+++ Workouts/src/app/_layout.jsx
@@ -1,8 +1,10 @@
 import { Stack } from "expo-router";
+import Navbar from "../components/Navbar";
 
 export default function RootLayout() {
   return (
     <Stack>
+      <Navbar />
       <Stack.Screen name="Index" options={{ title: "Exercises " }} />
     </Stack>
   );
