57487ef674b2f7f77e79bd3f4b92bfbb228ed71c
Sean Teams <shawntemesy@hotmail.com>
2024-10-05 04:58:58 -0300

    fixed typescript errors
    no more red

--- nextjs-extrasize-sesh-seans/lib/mongodb.ts
+++ nextjs-extrasize-sesh-seans/lib/mongodb.ts
@@ -1,12 +1,12 @@
 import { MongoClient } from 'mongodb'
 
-const uri = process.env.MONGODB_URI
+const uri = process.env.MONGODB_URI as string
 const options = {}
 
-let client
+let client: MongoClient
 let clientPromise: Promise<MongoClient>
 
-if (!process.env.MONGODB_URI) {
+if (!uri) {
   throw new Error('Please add your Mongo URI to .env.local')
 }
 
@@ -17,7 +17,7 @@ if (process.env.NODE_ENV === 'development') {
     client = new MongoClient(uri, options)
     global._mongoClientPromise = client.connect()
   }
-  clientPromise = global._mongoClientPromise
+  clientPromise = global._mongoClientPromise as Promise<MongoClient>
 } else {
   // In production mode, it's best to not use a global variable.
   client = new MongoClient(uri, options)
